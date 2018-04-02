a7827c5fce36263a1333878c522d1cdaad3a04cc
Utkarsh Barsaiyan <ubarsaiyan@gmail.com>
2018-04-03 02:41:35 +0530

    Added browserify support Clearing of output on pressing submit Improvements in chrono parser

--- nlp.js
+++ nlp.js
@@ -1,13 +1,12 @@
-var chrono = require('chrono-node');
-
 window.onload = function(){
 
 	$('.auto-save').savy('load');
-
+	
 	$('#submit-btn').on('click', function() {
 		let text = $('#text').val().split(/\.\s|\n/), i;
+		$('#result').html('');
 		for (i = 0; i < text.length; i++){
-			$('#result').append(chrono.parseDate(text[i])+'<br/>');
+			$('#result').append(chrono.parseDate(text[i], new Date(),{forwardDate: true})+'<br/>');
 		}
 	});
 }
